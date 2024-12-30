b6138756dea6242bd3ac67685fd0395c21a4971c
Muz-dev <113846204+Muz-dev@users.noreply.github.com>
2024-12-30 17:26:31 +0000

    Update HideAndSeekConfigMenu.cpp

--- source/server/hns/HideAndSeekConfigMenu.cpp
+++ source/server/hns/HideAndSeekConfigMenu.cpp
@@ -15,50 +15,10 @@ const sead::WFixedSafeString<0x200> *HideAndSeekConfigMenu::getStringData() {
     sead::SafeArray<sead::WFixedSafeString<0x200>, mItemCount>* gamemodeConfigOptions =
         new sead::SafeArray<sead::WFixedSafeString<0x200>, mItemCount>();
 
-    mItems->mBuffer[0].copy(u"Toggle H&S Gravity (OFF)");
-    mItems->mBuffer[1].copy(u"Mario Collision (ON)    ");
-    mItems->mBuffer[2].copy(u"Mario Bounce (ON)       ");
-    mItems->mBuffer[3].copy(u"Cappy Collision (OFF)   ");
-    mItems->mBuffer[4].copy(u"Cappy Bounce (OFF)      ");
-}
+    gamemodeConfigOptions->mBuffer[0].copy(u"Toggle H&S Gravity On");
+    gamemodeConfigOptions->mBuffer[1].copy(u"Toggle H&S Gravity Off");
 
-const sead::WFixedSafeString<0x200>* HideAndSeekConfigMenu::getStringData() {
-    // Gravity
-    const char16_t* gravity = (
-        HideAndSeekInfo::mIsUseGravity
-        ? u"Toggle H&S Gravity (ON) "
-        : u"Toggle H&S Gravity (OFF)"
-    );
-
-    // Collision Toggles
-    const char16_t* marioCollision = (
-        HideAndSeekInfo::mHasMarioCollision
-        ? u"Mario Collision (ON)    "
-        : u"Mario Collision (OFF)   "
-    );
-    const char16_t* marioBounce = (
-        HideAndSeekInfo::mHasMarioBounce
-        ? u"Mario Bounce (ON)       "
-        : u"Mario Bounce (OFF)      "
-    );
-    const char16_t* cappyCollision = (
-        HideAndSeekInfo::mHasCappyCollision
-        ? u"Cappy Collision (ON)    "
-        : u"Cappy Collision (OFF)   "
-    );
-    const char16_t* cappyBounce = (
-        HideAndSeekInfo::mHasCappyBounce
-        ? u"Cappy Bounce (ON)       "
-        : u"Cappy Bounce (OFF)      "
-    );
-
-    mItems->mBuffer[0].copy(gravity);
-    mItems->mBuffer[1].copy(marioCollision);
-    mItems->mBuffer[2].copy(marioBounce);
-    mItems->mBuffer[3].copy(cappyCollision);
-    mItems->mBuffer[4].copy(cappyBounce);
-
-    return mItems->mBuffer;
+    return gamemodeConfigOptions->mBuffer;
 }
 
 bool HideAndSeekConfigMenu::updateMenu(int selectIndex) {
